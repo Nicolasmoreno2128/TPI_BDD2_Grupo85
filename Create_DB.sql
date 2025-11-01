8bfd9a39a0c42e8f870e687d7d816a6e56fc38cd
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-11-01 12:19:37 -0300

    Marca y Mano_De_Obra
    Creación de tablas

--- Create_DB.sql
+++ Create_DB.sql
@@ -1,7 +1,26 @@
-Create database TPI_BDD2_Grupo85
+--Create database TPI_BDD2_Grupo85
 
-go
+--go
 
 use TPI_BDD2_Grupo85
 
 go
+
+--Nico
+create table Marca(
+IdMarca int identity (1,1) primary key,
+Nombre Varchar (50) not null
+)
+go
+--Nico
+create table Mano_De_Obra(
+IdManoDeObra int identity (1,1) primary key,
+Descripcion varchar (255) not null,
+PrecioPorHora decimal not null
+)
+go
+
+
+
+
+
