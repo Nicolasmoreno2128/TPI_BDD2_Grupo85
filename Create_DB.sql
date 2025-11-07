c3ec81c409f307ead0afb10d4da4ea180168cc9e
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-11-06 23:24:29 -0300

    Creación de tablas Cliente, Turnos y Service_Detalle_x_Tipo_Service
    Se crean la tablas:
    Cliente, Turnos y Service_Detalle_x_Tipo_Service
    Además de las creaciones de las tablas se organiza el orden de las tablas para que las creaciones se puedan dar de forma correcta.

--- Create_DB.sql
+++ Create_DB.sql
@@ -1,6 +1,6 @@
---Create database TPI_BDD2_Grupo85
+Create database TPI_BDD2_Grupo85
 
---go
+go
 
 use TPI_BDD2_Grupo85
 
@@ -13,10 +13,60 @@ Nombre Varchar (50) not null
 )
 go
 --Nico
-create table Mano_De_Obra(
+create table ManoDeObra(
 IdManoDeObra int identity (1,1) primary key,
 Descripcion varchar (255) not null,
-PrecioPorHora decimal not null
+PrecioPorHora decimal (10,2) not null
+)
+go
+--Maia
+CREATE TABLE TipoService (
+    IdTipoService INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Descripcion VARCHAR(255)
+)
+GO
+
+CREATE TABLE Repuestos(
+    IdRepuesto INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Stock INT NOT NULL,
+    PrecioUnitario DECIMAL (10,2) NOT NULL
+)
+go
+
+CREATE TABLE Pagos (
+    IdPago INT PRIMARY KEY IDENTITY (1,1),
+    FechaPago DATETIME NOT NULL,
+    Monto DECIMAL (10,2) CHECK (Monto > 0),
+    MetodoPago VARCHAR (100) NOT NULL
+)
+
+go
+
+CREATE TABLE Rol (
+    IdRol INT PRIMARY KEY IDENTITY (1,1),
+    Tipo VARCHAR (100) NOT NULL UNIQUE
+);
+go
+
+CREATE TABLE Modelo (
+    IdModelo INT IDENTITY(1,1) PRIMARY KEY,
+    IdMarca INT NOT NULL,
+    Nombre VARCHAR(50) NOT NULL,
+    FOREIGN KEY (IdMarca) REFERENCES Marca(IdMarca)
+)
+GO
+
+-- Santiago
+CREATE TABLE Persona(
+    IdPersona INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    DNI VARCHAR(13) NOT NULL,
+    Direccion VARCHAR(200) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    Email VARCHAR(100) NOT NULL
 )
 go
 
@@ -31,6 +81,18 @@ CREATE TABLE Usuario (
     FOREIGN KEY (IdRol) REFERENCES Rol(IdRol)
 )
 GO
+
+--Nico
+CREATE TABLE Cliente(
+IdCliente int identity (1,1) primary key,
+IdPersona int not null,
+FechaAlta datetime not null,
+foreign key (IdPersona) references Persona(IdPersona)
+
+)
+
+go
+
 --Maia
 CREATE TABLE Empleado (
     IdEmpleado INT IDENTITY(1,1) PRIMARY KEY,
@@ -39,21 +101,6 @@ CREATE TABLE Empleado (
     Sueldo DECIMAL(10,2) NOT NULL,
     FechaIngreso DATETIME NOT NULL,
     FOREIGN KEY (IdPersona) REFERENCES Persona(IdPersona)
-)
-GO
---Maia
-CREATE TABLE Modelo (
-    IdModelo INT IDENTITY(1,1) PRIMARY KEY,
-    IdMarca INT NOT NULL,
-    Nombre VARCHAR(50) NOT NULL,
-    FOREIGN KEY (IdMarca) REFERENCES Marca(IdMarca)
-)
-GO
---Maia
-CREATE TABLE TipoService (
-    IdTipoService INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre VARCHAR(255) NOT NULL,
-    Descripcion VARCHAR(255)
 )
 GO
 
@@ -67,41 +114,23 @@ CREATE TABLE Vehiculo (
     FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
     FOREIGN KEY (IdModelo) REFERENCES Modelo(IdModelo)
 );
-
-CREATE TABLE Rol (
-    IdRol INT PRIMARY KEY IDENTITY (1,1),
-    Tipo VARCHAR (100) NOT NULL UNIQUE
-);
-
-CREATE TABLE Pagos (
-    IdPago INT PRIMARY KEY IDENTITY (1,1),
-    FechaPago DATETIME NOT NULL,
-    Monto DECIMAL (10,2) CHECK (Monto > 0),
-    MetodoPago VARCHAR (100) NOT NULL
-);
-
-CREATE TABLE ServiceDetalle_X_Repuestos (
-    IdServiceDetalle INT NOT NULL,
-    IdRepuesto INT NOT NULL,
-    Cantidad INT NOT NULL CHECK (Cantidad > 0),
-    PRIMARY KEY (IdServiceDetalle, IdRepuesto),
-    FOREIGN KEY (IdServiceDetalle) REFERENCES Service_Detalle (IdServiceDetalle),
-    FOREIGN KEY (IdRepuesto) REFERENCES Repuesto (IdRepuesto)
-);
-
--- Santiago
-CREATE TABLE Persona(
-    IdPersona INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    Nombre VARCHAR(100) NOT NULL,
-    Apellido VARCHAR(100) NOT NULL,
-    DNI VARCHAR(13) NOT NULL,
-    Direccion VARCHAR(200) NOT NULL,
-    FechaNacimiento DATE NOT NULL,
-    Email VARCHAR(100) NOT NULL
-)
 go
 
-CREATE TABLE Service(
+--Nico
+CREATE TABLE Turnos(
+IdTurno int identity (1,1) primary key,
+IdVehiculo int not null,
+IdEmpleados int not null,
+FechaTurno datetime not null,
+Estado varchar(20) not null,
+Observaciones text not null,
+foreign key (IdVehiculo) references Vehiculo(IdVehiculo),
+foreign key (IdEmpleados) references Empleado(IdEmpleado)
+)
+
+go 
+
+CREATE TABLE Service_(
     IdService INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
     IdTurno INT NOT NULL FOREIGN KEY REFERENCES Turnos(IdTurno), 
     IdEmpleado INT NOT NULL FOREIGN KEY REFERENCES Empleado(IdEmpleado), 
@@ -113,17 +142,33 @@ CREATE TABLE Service(
 )
 go
 
-CREATE TABLE Repuestos(
-    IdRepuesto INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    Nombre VARCHAR(100) NOT NULL,
-    Stock INT NOT NULL,
-    PrecioUnitario DECIMAL (10,2) NOT NULL
+CREATE TABLE Service_Detalle(
+    IdServiceDetalle INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    IdService INT NOT NULL FOREIGN KEY REFERENCES Service_(IdService),
+    IdManoDeObra INT NOT NULL FOREIGN KEY REFERENCES ManoDeObra(IdManoDeObra)
 )
 go
 
-CREATE TABLE Service_Detalle(
-    IdServiceDetalle INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    IdService INT NOT NULL FOREIGN KEY REFERENCES Service(IdService),
-    IdManoDeObra INT NOT NULL FOREIGN KEY REFERENCES Mano_De_Obra(IdManoDeObra)
-)
+CREATE TABLE ServiceDetalle_X_Repuestos (
+    IdServiceDetalle INT NOT NULL,
+    IdRepuesto INT NOT NULL,
+    Cantidad INT NOT NULL CHECK (Cantidad > 0),
+    PRIMARY KEY (IdServiceDetalle, IdRepuesto),
+    FOREIGN KEY (IdServiceDetalle) REFERENCES Service_Detalle (IdServiceDetalle),
+    FOREIGN KEY (IdRepuesto) REFERENCES Repuestos (IdRepuesto)
+);
+
 go
+
+CREATE TABLE Service_Detalle_x_Tipo_Service (
+IdServiceDetalle int not null,
+IdTipoService int not null,
+primary key (IdServiceDetalle,IdTipoService),
+foreign key (IdServiceDetalle) references Service_Detalle (IdServiceDetalle),
+foreign key (IdTipoService) references TipoService (IdTipoService),
+
+
+)
+
+
+
