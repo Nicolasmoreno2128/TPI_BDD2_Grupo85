6ca6320f13f65f42c07b5b644af3492e3f48ac28
julianfparodi <julianfacundoparodi@gmail.com>
2025-11-15 18:55:31 -0300

    Se saca el try catch del trigger evitar turnos duplicados

--- Triggers.sql
+++ Triggers.sql
@@ -10,32 +10,17 @@ INSTEAD OF INSERT
 AS
 BEGIN
 
-    BEGIN TRY
-        BEGIN TRANSACTION;
-
         IF EXISTS (SELECT 1 FROM Turno T 
         INNER JOIN inserted I ON T.IdVehiculo = I.IdVehiculo AND T.FechaTurno = I.FechaTurno
         )
         BEGIN
             RAISERROR('Ya existe un turno asignado para este vehículo en esa fecha y hora.', 16, 1);
-            ROLLBACK TRANSACTION;
             RETURN;
         END;
-
 
         INSERT INTO Turno (IdVehiculo, IdEmpleado, FechaTurno, Estado, Observaciones)
         SELECT IdVehiculo, IdEmpleado, FechaTurno, Estado, Observaciones
         FROM inserted;
-
-        COMMIT TRANSACTION;
-    END TRY
-
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0
-            ROLLBACK TRANSACTION;
-
-        RAISERROR('Ocurrio un error', 16, 1);
-    END CATCH
 END;
 GO
 
